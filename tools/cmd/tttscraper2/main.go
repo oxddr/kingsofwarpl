4c9717767eafb7c58fd7cbdab93f4b8277b99f9d
Janek Łukaszewicz <janluk@google.com>
2022-02-28 13:57:15 +0100

    Add tttscraper2 - tool to scrape ttt and save it to sqlite database.

---
+++ tools/cmd/tttscraper2/main.go
@@ -0,0 +1,99 @@
+package main
+
+import (
+	"database/sql"
+	"flag"
+	"fmt"
+	"log"
+
+	"github.com/oxddr/kingsofwarpl/tools/model"
+	"github.com/oxddr/kingsofwarpl/tools/tttscraper"
+
+	_ "github.com/mattn/go-sqlite3"
+)
+
+var (
+	databasePath = flag.String("database_path", "", "Path to Sqlite database")
+	eventID      = flag.String("event_id", "", "Event ID")
+	eventURL     = flag.String("event_url", "", "URL of the event on tabletop.to")
+	dryRun       = flag.Bool("dry_run", true, "If true, no changes to database are made")
+)
+
+func save(results *model.TournamentResults) error {
+	db, err := sql.Open("sqlite3", *databasePath)
+	if err != nil {
+		return fmt.Errorf("unable to open database from %q: %v", *databasePath, err)
+	}
+
+	tx, err := db.Begin()
+	if err != nil {
+		return fmt.Errorf("unable to open transaction: %v", err)
+	}
+
+	// resultStmt, err := tx.Prepare("INSERT INTO Results(player, event, faction, tp, bonus_tp, attrition_points) values(?,?,?,?,?,?)")
+	// if err != nil {
+	// 	return fmt.Errorf("unable to prepare statement: %v", err)
+	// }
+
+	playerStmt, err := tx.Prepare("INSERT INTO Players(tabletop_id, name) values(?, ?)")
+	if err != nil {
+		return fmt.Errorf("unable to prepate statement: %v", err)
+	}
+
+	for _, p := range results.Players {
+		_, err := playerStmt.Exec(p.ID, p.Name)
+		if err != nil {
+			rErr := tx.Rollback()
+			if rErr != nil {
+				return fmt.Errorf("unable to rollback transaction: %v (rollback-causing error: %v)", rErr, err)
+			}
+			return fmt.Errorf("unable to save Player %q: %v", p.ID, err)
+		}
+
+		// _, err = resultStmt.Exec(p.ID, *eventID, p.Faction, p.TP, p.BonusTP, p.AttritionPoints)
+		// if err != nil {
+		// 	rErr := tx.Rollback()
+		// 	if rErr != nil {
+		// 		return fmt.Errorf("unable to rollback transaction: %v (rollback-causing error: %v)", rErr, err)
+		// 	}
+		// 	return fmt.Errorf("unable to save Result %v: %v", p, err)
+		// }
+	}
+	err = tx.Commit()
+	if err != nil {
+		rErr := tx.Rollback()
+		if rErr != nil {
+			return fmt.Errorf("unable to rollback transaction: %v (rollback-causing error: %v)", rErr, err)
+		}
+		return fmt.Errorf("unable to commit transaction: %v", err)
+	}
+	return nil
+}
+
+func descResults(results *model.TournamentResults) string {
+	res := fmt.Sprintf("Tournament: %s\n", results.Tournament.Name)
+	res += "id faction tp bonus_tp attr\n"
+	for _, p := range results.Players {
+		res += fmt.Sprintf("%s %s %d %d %d\n", p.ID, p.Faction, p.TP, p.BonusTP, p.AttritionPoints)
+	}
+	return res
+}
+
+func main() {
+	flag.Parse()
+
+	results, err := tttscraper.Scrape(*eventURL)
+	if err != nil {
+		log.Fatalf("Unable to scraper %q: %v", *eventURL, err)
+	}
+
+	log.Print(descResults(results))
+
+	if !*dryRun {
+		log.Println("Saving to database...")
+		err := save(results)
+		if err != nil {
+			log.Fatalf("Unable to save in database: %v", err)
+		}
+	}
+}
--- tools/go.mod
+++ tools/go.mod
@@ -5,13 +5,14 @@ go 1.17
 require (
 	bitbucket.org/creachadair/stringset v0.0.10
 	github.com/PuerkitoBio/goquery v1.8.0
+	github.com/mattn/go-sqlite3 v1.14.11
+	github.com/spf13/pflag v1.0.5
 	go.uber.org/multierr v1.7.0
 	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
 )
 
 require (
 	github.com/andybalholm/cascadia v1.3.1 // indirect
-	github.com/spf13/pflag v1.0.5 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
 )
