0fd1e45d349e8e1bd4510bf926ce3d14b8f09f51
Janek Łukaszewicz <janluk@google.com>
2022-03-01 10:32:15 +0100

    Provide default database in tttscraper2

--- tools/cmd/tttscraper2/main.go
+++ tools/cmd/tttscraper2/main.go
@@ -13,7 +13,7 @@ import (
 )
 
 var (
-	databasePath = flag.String("database_path", "", "Path to Sqlite database")
+	databasePath = flag.String("database_path", "kingsofwar-pl.sqlite3", "Path to Sqlite database")
 	eventID      = flag.String("event_id", "", "Event ID")
 	eventURL     = flag.String("event_url", "", "URL of the event on tabletop.to")
 	dryRun       = flag.Bool("dry_run", true, "If true, no changes to database are made")
@@ -30,10 +30,10 @@ func save(results *model.TournamentResults) error {
 		return fmt.Errorf("unable to open transaction: %v", err)
 	}
 
-	// resultStmt, err := tx.Prepare("INSERT INTO Results(player, event, faction, tp, bonus_tp, attrition_points) values(?,?,?,?,?,?)")
-	// if err != nil {
-	// 	return fmt.Errorf("unable to prepare statement: %v", err)
-	// }
+	resultStmt, err := tx.Prepare("INSERT INTO Results(player, event, faction, tp, bonus_tp, attrition_points) values(?,?,?,?,?,?)")
+	if err != nil {
+		return fmt.Errorf("unable to prepare statement: %v", err)
+	}
 
 	playerStmt, err := tx.Prepare("INSERT INTO Players(tabletop_id, name) values(?, ?)")
 	if err != nil {
@@ -50,14 +50,14 @@ func save(results *model.TournamentResults) error {
 			return fmt.Errorf("unable to save Player %q: %v", p.ID, err)
 		}
 
-		// _, err = resultStmt.Exec(p.ID, *eventID, p.Faction, p.TP, p.BonusTP, p.AttritionPoints)
-		// if err != nil {
-		// 	rErr := tx.Rollback()
-		// 	if rErr != nil {
-		// 		return fmt.Errorf("unable to rollback transaction: %v (rollback-causing error: %v)", rErr, err)
-		// 	}
-		// 	return fmt.Errorf("unable to save Result %v: %v", p, err)
-		// }
+		_, err = resultStmt.Exec(p.ID, *eventID, p.Faction, p.TP, p.BonusTP, p.AttritionPoints)
+		if err != nil {
+			rErr := tx.Rollback()
+			if rErr != nil {
+				return fmt.Errorf("unable to rollback transaction: %v (rollback-causing error: %v)", rErr, err)
+			}
+			return fmt.Errorf("unable to save Result %v: %v", p, err)
+		}
 	}
 	err = tx.Commit()
 	if err != nil {
