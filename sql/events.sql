c6d06c6850d7a98bb92058d52e549b3529a602c9
Janek Łukaszewicz <janluk@google.com>
2022-03-08 14:46:38 +0100

    Add "4 turniej ligi"

--- sql/events.sql
+++ sql/events.sql
@@ -1,10 +1,12 @@
 SELECT name,
        date,
+       end_date,
        city,
        location,
        format,
        website AS event_url,
-       tabletop_url AS signup_url
+       tabletop_url AS signup_url,
+       notes
   FROM Events
  WHERE date > '2021-12-31'
  ORDER BY 2 DESC;
